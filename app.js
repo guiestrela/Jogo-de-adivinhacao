9555940667dcad573baa5c9b8a7f098ea857926f
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-09-29 20:43:36 -0300

    update

---
+++ app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogodo numero secreto'
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um numero entre 1 e 10'
