c356289092dfcdb49639dd44172e21c3dd6c3876
Guilherme Estrela Lambert de Brito <guilhermestrela@gmail.com>
2023-10-03 16:49:27 -0300

    update js

--- app.js
+++ app.js
@@ -1,19 +1,53 @@
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'jogodo numero secreto');
-exibirTextoNaTela('p','Escolha um numero entre 1 e 10');
+function exibirMessagemInicial() {
+    exibirTextoNaTela('h1', 'jogodo numero secreto');
+    exibirTextoNaTela('p','Escolha um numero entre 1 e 10');
+
+}
+
+exibirMessagemInicial();
 
 
 function verificarChute() {
-    let chute = document.querySelector('input').value
-    console.log(chute == numeroSecreto);
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns, você acertou');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled')
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número é menor');
+        } else {
+        exibirTextoNaTela('p', 'O número é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMessagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled',true)
 }
